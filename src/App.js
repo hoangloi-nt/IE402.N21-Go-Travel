026b601ebf1b035834ac08d1a148fd6ff95688bb
ThanhLamUit <thanhlam.6178@gmail.com>
2023-06-04 20:49:44 +0700

    Tour Details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 const HomePage = lazy(() => import("pages/HomePage"));
+const TourDetails = lazy(() => import("pages/TourDetails"));
 
 function App() {
   return (
     <Suspense>
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/location" element={<TourDetails></TourDetails>}></Route>
       </Routes>
     </Suspense>
   );
