ff70cec59bec752c5dd3dbec9c08f2fa0f2b749c
hungha <hungha@lecle.co.kr>
2023-06-17 17:40:12 +0700

    chi tiết địa điểm

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import FamousSitePage from "pages/FamousSitePage";
 
 const HomePage = lazy(() => import("pages/HomePage"));
 const TourDetails = lazy(() => import("pages/TourDetails"));
+const ChiTietDiaDiem = lazy(() => import("pages/ChiTietDiaDiem"));
 
 function App() {
 	return (
 		<Suspense>
 			<Routes>
-				<Route path="/" element={<HomePage></HomePage>}></Route>
-				<Route path="/location" element={<TourDetails></TourDetails>}></Route>
-				<Route path="/manage-tour" element={<ManageTour></ManageTour>}></Route>
+				<Route path="/" element={<HomePage></HomePage>} />
+				<Route path="/location" element={<TourDetails></TourDetails>} />
+				<Route path="/manage-tour" element={<ManageTour></ManageTour>} />
 				<Route
 					path="/famous-site"
 					element={<FamousSitePage></FamousSitePage>}
-				></Route>
+				/>
+				<Route path="/chitiet" element={<ChiTietDiaDiem></ChiTietDiaDiem>} />
 			</Routes>
 		</Suspense>
 	);
